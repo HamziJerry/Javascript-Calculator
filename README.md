758283a451e38febf92fbdb5e0da358ab004ba4f
Hamza <90974206+HamziJerry@users.noreply.github.com>
2022-05-27 20:05:12 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculator
+
+Fully Responsive Working Javascript Calculator with Custom Beautiful Design. Normal functionalities implemented in calculator like addition, subtraction, multiplication and division etc by using eval function.
+
+# Live Working Project Url
+
+
---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("This is javascript calculator project");
+
+let screen = document.getElementById("screen");
+let buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for(item of buttons){
+    item.addEventListener('click', (e) => {
+        let buttonText = e.target.innerText;
+        console.log("Button you click is: ", buttonText);
+        if(buttonText == "x"){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }else if(buttonText == "AC"){
+            screenValue = ""
+            screen.value = screenValue;
+        }else if(buttonText == "="){
+            screen.value = eval(screenValue);
+        }else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
